fe88ed0a7b0cd9621525a1ea1aaacd1263298b9f
ThanhNguyen11 <hthanh1192@gmail.com>
2023-02-10 20:44:20 +0700

    create bot

---
+++ telebot/credentials.py
@@ -0,0 +1,7 @@
+# bot_token = "here goes your access token from BotFather"
+# bot_user_name = "the username you entered"
+# URL = "the heroku app link that we will create later"
+
+bot_token = "6188485731:AAGpwcAsJM7OAYt-BbE_czyRID3EAOZ70kU"
+bot_user_name = "ThanhDevBot"
+URL = "the heroku app link that we will create later"
